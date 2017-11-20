be1d1c01180e52874147d251b29f8cd5eae0787c
David Hagan <dave@stackableregiments.com>
2017-11-20 18:59:44 +1100

    didn't find the rails applicationcontroller, so tweaking around that

--- plugin.rb
+++ plugin.rb
@@ -17,62 +17,62 @@ module ::EnhancedLogoutEndpoint
 		engine_name PLUGIN_NAME
  		isolate_namespace EnhancedLogoutEndpoint
 	end
+end
 
-	class EnhancedLogoutController < Rails::ApplicationController
-		layout :false
-		skip_before_action :redirect_to_login_if_required
-		
-		def generateRedirectScript
-			redirectUrl = SiteSettings.enhancedlogout_redirectUrl
-			<<-EMBEDDEDSCRIPT
-<script type="text/javascript">
-	var redirectUrl = "#{redirectUrl}";
-	console.log("redirectingTo:",redirectUrl);
-</script>
-			EMBEDDEDSCRIPT
-		end	
-
-		def generateCookieRemovalScript
-			<<-EMBEDDEDSCRIPT
-<script type="text/javascript">
-	console.log("removing cookies");
-</script>
-			EMBEDDEDSCRIPT
-		end	
-
-		def generateBrowserBackScript
-			<<-EMBEDDEDSCRIPT
-<script type="text/javascript">
-	console.log("clearing browser session history");
-</script>
-			EMBEDDEDSCRIPT
-		end	
-
-		def customPageContent
-			SiteSettings.enhancedlogout_customLogoutPageHtml
-		end	
-
-		def performPostLogout
-			renderedPage = '<html><head>'
-			if SiteSettings.enhancedlogout_shouldClearCookies? 
-				renderedPage = renderedPage + generateCookieRemovalScript
-			end	
-			if SiteSettings.enhancedlogout_shouldClearBrowserSessionHistory? 
-				renderedPage = renderedPage + generateBrowserBackScript
-			end	
-			if SiteSettings.enhancedlogout_shouldRedirect? 
-				renderedPage = renderedPage + generateCookieRemovalScript
-			end	
-			renderedPage = renderedPage + '</head><body>#{customPageContent}</body></html>'
-			render :inline => renderedPage.html_safe
-		end	
-	end
-
-	EnhancedLogoutEndpoint::Engine.routes.draw do
-		get "/" => "enhancedLogout#performPostLogout"
-	end	
+class EnhancedLogoutController < ::Rails::ApplicationController
+	layout :false
+	skip_before_action :redirect_to_login_if_required
 	
-	Discourse::Application.routes.append do
-		mount ::EnhancedLogoutEndpoint::Engine, at: "/enhanced-logout"
+	def generateRedirectScript
+		redirectUrl = SiteSettings.enhancedlogout_redirectUrl
+		<<-EMBEDDEDSCRIPT
+<script type="text/javascript">
+var redirectUrl = "#{redirectUrl}";
+console.log("redirectingTo:",redirectUrl);
+</script>
+		EMBEDDEDSCRIPT
 	end	
+
+	def generateCookieRemovalScript
+		<<-EMBEDDEDSCRIPT
+<script type="text/javascript">
+console.log("removing cookies");
+</script>
+		EMBEDDEDSCRIPT
+	end	
+
+	def generateBrowserBackScript
+		<<-EMBEDDEDSCRIPT
+<script type="text/javascript">
+console.log("clearing browser session history");
+</script>
+		EMBEDDEDSCRIPT
+	end	
+
+	def customPageContent
+		SiteSettings.enhancedlogout_customLogoutPageHtml
+	end	
+
+	def performPostLogout
+		renderedPage = '<html><head>'
+		if SiteSettings.enhancedlogout_shouldClearCookies? 
+			renderedPage = renderedPage + generateCookieRemovalScript
+		end	
+		if SiteSettings.enhancedlogout_shouldClearBrowserSessionHistory? 
+			renderedPage = renderedPage + generateBrowserBackScript
+		end	
+		if SiteSettings.enhancedlogout_shouldRedirect? 
+			renderedPage = renderedPage + generateCookieRemovalScript
+		end	
+		renderedPage = renderedPage + '</head><body>#{customPageContent}</body></html>'
+		render :inline => renderedPage.html_safe
+	end	
+end
+
+EnhancedLogoutEndpoint::Engine.routes.draw do
+	get "/" => "enhancedLogout#performPostLogout"
+end	
+
+Discourse::Application.routes.append do
+	mount ::EnhancedLogoutEndpoint::Engine, at: "/enhanced-logout"
 end	
